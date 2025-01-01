d4e806cfc18624cca09d76fd9cc7e8906aaeda3b
max.abimbola <max.abimbola@datatonic.com>
2025-01-01 13:29:42 +0100

    feat: created function to download csv from kaggle dataset to local stoage

--- a_data_ingestion/data_ingestion.py
+++ a_data_ingestion/data_ingestion.py
@@ -2,8 +2,34 @@
 A script to load the raw 'used-car-dataset' data into a cloud storage bucket and then into a big query dataset
 """
 
+import kagglehub
+import pandas as pd
+import numpy as np
+import os
+import os.path
+import logging
+from google.cloud import storage
+
+def download_kaggle_dataset():
+    existing_csv_file_path = '/Users/max.abimbola/.cache/kagglehub/datasets/austinreese/craigslist-carstrucks-data/versions/10'  
+    if os.path.isfile(existing_csv_file_path):
+        logging.info('file already exists...')
+        return
+
+
+    kaggle_dataset_path = kagglehub.dataset_download("austinreese/craigslist-carstrucks-data")
+    logging.info('dataest succesfully downloaded from kaggle')
+    print("Path to dataset files:", kaggle_dataset_path)
+
+
+def upload_csv_to_storage_blob():
+    path = '/Users/max.abimbola/.cache/kagglehub/datasets/austinreese/craigslist-carstrucks-data/versions/10'  
+    df = pd.read_csv(path)
+    print(df.head())
+
 def main():
-    return 0
+    download_kaggle_dataset()
+    upload_csv_to_storage_blob()
 
 if __name__ == '__main__':
     main()
