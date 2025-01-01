4a6f70f76980405167c5ae7e3c3778d6e622590c
max.abimbola <max.abimbola@datatonic.com>
2025-01-01 12:53:37 +0100

    first commit

---
+++ a_data_ingestion/data_ingestion.py
@@ -0,0 +1,9 @@
+"""
+A script to load the raw 'used-car-dataset' data into a cloud storage bucket and then into a big query dataset
+"""
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
