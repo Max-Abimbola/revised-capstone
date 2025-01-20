03e963a551537e07bee6bb5069f075c916567d87
max.abimbola <max.abimbola@datatonic.com>
2025-01-20 10:53:25 +0000

    wrote logic for creating batches of 50 VINs

--- b_data_enrichment/data_enrichment.py
+++ b_data_enrichment/data_enrichment.py
@@ -1,7 +1,10 @@
 from google.cloud import bigquery
 from google.cloud import storage
+import requests
+import math
 
-def extract_missing_vins():
+
+def return_extracted_vins():
     client = bigquery.Client(project="dt-maxa-sandbox-dev")
     query = """
                 SELECT distinct VIN FROM `dt-maxa-sandbox-dev.uncleaned_data.raw_car_data`
@@ -19,10 +22,41 @@ def extract_missing_vins():
     results = query_job.result().to_dataframe()
     csv_string = results.to_csv(index=False)
     vin_list = csv_string.split('\n')[2:]
-    print(vin_list[0])
+    return vin_list
+
+def return_updated_values(delimitted_vin_list):
+    url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/1C6RR6FG0JS259587;5TFCZ5AN0KX185798'
+    post_fields = {'format': 'json', 'data':delimitted_vin_list}
+    r = requests.post(url, data=post_fields)
+    response_dict = dict(r.json())['Results']
+    
+    new_values_dict = []
+    for vin in response_dict:
+        curr_val = {}
+        curr_val['VIN'] = vin.get('VIN')
+        curr_val['cylinders'] = vin.get('EngineCylinders','')
+        curr_val['drive'] = vin.get('DriveType',''),
+        curr_val['fuel_type'] = vin.get('FuelTypePrimary','')
+        curr_val['make'] = vin.get('Make','')
+        curr_val['vehicle_type'] = vin.get('VehicleType','')
+        
+        new_values_dict.append(curr_val)
+    return(new_values_dict)
+
+
+
 
 def main():
-    extract_missing_vins()
+    vin_list = return_extracted_vins()[:156]
+
+    while len(vin_list) != 0:
+        batch_vin_list = vin_list[0:min(50,len(vin_list))]
+
+        vin_list = vin_list[min(50,len(vin_list)):]
+
+        # updated_values = return_updated_values(delimitted_vin_list)
+
+    
 
 if __name__ == "__main__":
     main()
