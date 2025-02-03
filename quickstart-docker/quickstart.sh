86c4cd68f3d467d87ca0ea7eb2fc78ff428a8b83
max.abimbola <max.abimbola@datatonic.com>
2025-02-03 14:24:16 +0000

    fix: modified yaml file to specify logging location

--- quickstart-docker/quickstart.sh
+++ quickstart-docker/quickstart.sh
@@ -1,1 +1,28 @@
 echo "Hello world! The time is $(date)."
+
+
+
+gcloud builds triggers create github \                                            
+--region=europe-west2 \
+--repo-name=revised-capstone \
+--repo-owner=maxabimbola \
+--branch-pattern=data_enrichment \
+--build-config=quickstart-docker/cloudbuild.yaml \
+--service-account=max-dev@dt-maxa-sandbox-dev.iam.gserviceaccount.com
+
+gcloud builds triggers create github \                    
+--region=europe-west2 \                                          
+--repo-name=revised-capstone \
+--repo-owner=maxabimbola \
+--branch-pattern=data_enrichment \
+--build-config=quickstart-docker/cloudbuild.yaml \
+--service-account=max-dev@dt-maxa-sandbox-dev.iam.gserviceaccount.com \
+
+
+    gcloud builds triggers create github \
+    --region=europe-west2 \
+    --repo-name=https://github.com/Max-Abimbola/revised-capstone \
+    --repo-owner=maxabimbola \
+    --branch-pattern=data_enrichment \
+    --build-config=quickstart-docker/cloudbuild.yaml \
+    --service-account=projects/dt-maxa-sandbox-dev/serviceAccounts/max-dev@dt-maxa-sandbox-dev.iam.gserviceaccount.com
