01533ff8872bce6f6fce3c9840e3baf350744109
max.abimbola <max.abimbola@datatonic.com>
2025-02-04 18:25:10 +0000

    feat: successfully implemented CI/CD for cloud run function

--- b_data_enrichment/main.py
+++ b_data_enrichment/main.py
@@ -41,6 +41,7 @@ def return_extracted_vins():
 
 
 def return_updated_values(batch_vin_list):
+    print("Test: CI/CD!!!!!!!!!!!!!!!!!!!!!")
     """
     This function takes a batch of VINs and retrieves their corresponding
     vehicle data (cylinders, drive type, fuel type, make, vehicle type)
